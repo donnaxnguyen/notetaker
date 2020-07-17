fa36a3aa195da8777b2a6eb5635a3d50aedf83a5
donna <donnaxnguyen@yahoo.com>
2020-07-16 22:23:13 -0700

    added routes and started server.js, also fixed the notes and index html folder

---
+++ server.js
@@ -0,0 +1,20 @@
+// creating paths
+const path = require('path');
+const fs = require('fs');
+const PORT = process.env.PORT || 3333;
+
+// need to set up express app to parse data
+
+// need get request 
+
+// need post request 
+
+// need delete request
+
+// folder to get the css and js file 
+
+
+// set up my port 
+app.listen(PORT, function () {
+    console.log("Please visit http://localhost: " + PORT +" enjoy your stay!");
+});
