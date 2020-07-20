fc4ec12524ae6e9f7f07b03ef56b723d3f66a123
donna <donnaxnguyen@yahoo.com>
2020-07-19 22:08:31 -0700

    finishing notes and api

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,16 +1,36 @@
 // work in progress //
 
-// Connect routes to my data 
-var journalData = require("../data/data");
+// connect routes to my data 
+var noteData = require("../data/data");
 
-// Routing
+// routing
 module.exports = function (app) {
 
-  // GET request
+
+  // get request
+  app.get("/api/notes", function (req, res) {
+    res.json(noteData);
+  });
 
   // POST request
-
+  app.post("/api/notes", function (req, res) {
+    noteData.push(req.body);
+    res.json("saved");
+  });
 
   // DELETE request
+  app.delete("/api/notes/:index", function (req, res) {
+    var elem = parseInt(req.params.index);
+    var tempnote = [];
+    for (var i = 0; i < noteData.length; i++) {
+      if (i !== elem) {
+        tempnote.push(noteData[i]);
+      }
+    }
+    noteData = tempnote;
+
+    res.json("delete completed");
+  });
+
 
 }
